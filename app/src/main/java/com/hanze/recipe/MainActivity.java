e7c635c37f3f3498d4bc6026384fe67a0343d2d5
Tim Perdok <Onmogeloos@users.noreply.github.com>
2020-06-08 15:35:36 +0200

    [Add] Lege app start

---
+++ app/src/main/java/com/hanze/recipe/MainActivity.java
@@ -0,0 +1,21 @@
+package com.hanze.recipe;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
